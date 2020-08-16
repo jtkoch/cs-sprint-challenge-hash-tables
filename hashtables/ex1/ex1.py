d9d285158fc53050491713db180f162fc74c7bd9
jtkoch <jtkoch12@gmail.com>
2020-08-16 18:32:12 -0500

    ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,13 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    cache = {} # dict
+    for i in range(len(weights)): # loop over lists of weights
+        cache[weights[i]] = i # store each weights list index as its value
+
+    for i in range(len(weights)):
+        difference = limit - weights[i]
+        if difference in cache:
+            return (max(i, cache[difference]), min(i, cache[difference]))    
+
     return None
